69c984297611f6452bd6154f04a8a542f440aab9
NonozgYtb <nonozg.ytb@gmail.com>
2019-12-23 19:37:53 +0100

    Retour a la racine du site !

--- include.php
+++ include.php
@@ -1,7 +1,20 @@
 <?php
-function footer() {
-    include("./startbootstrap-agency-gh-pages/footer.inc.php");
-};
-function hheader() {
-    include("./startbootstrap-agency-gh-pages/header.inc.php");
+function bdd()
+{
+    $host = "localhost";
+    $dbname = "devncie";
+    $user = "root";
+    $password = ""; // Sur Wamp, il n'y a pas de mot de passe.
+    $sql = 'mysql:host=' . $host . '; dbname=' . $dbname;
+    $bdd = new PDO($sql, $user, $password);
+    return $bdd;
 }
+function footer()
+{
+    include("./footer.inc.php");
+};
+function hheader()
+{
+    include("./header.inc.php");
+}
+bdd();
