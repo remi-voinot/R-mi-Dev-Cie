7d6947866de3d6912b815f90e259ae86d541608b
NonozgYtb <nonozg.ytb@gmail.com>
2019-12-23 20:24:46 +0100

    Systeme SQL de création de compte valide
    - Fin du systeme de création de compte dans la database.
    - Fin de la mise en place de restriction a cause d'informations non
      entrées ou non accpetées
    /!\ ATTENTION : MOT DE PASSE NON CRYPTE ! /!\

--- signintraitement.php
+++ signintraitement.php
@@ -36,15 +36,27 @@ function signin($post, $bdd)
         error('J\'accepte les CGUV');
         $valid = false;
     }
+    if ($post['mdp'] != $post['cmdp']) {
+        echo ("Les champs Mot de passe et Confirmation du mot de passe ne sont pas identiques");
+        $valid = false;
+    }
+    if (strpos($post['mdp'], " ") || strpos($post['mdp'], "_") || strpos($post['mdp'], "-")) {
+        echo ("Le mot de passe ne doit pas contenir d'espace, l'underscore (_) ou de tiret (-)");
+        $valid = false;
+    }
+    if (strpos($post['pseudo'], " ") || strpos($post['pseudo'], "_") || strpos($post['pseudo'], "-")) {
+        echo ("Le pseudo ne doit pas contenir d'espace, l'underscore (_) ou de tiret (-)");
+        $valid = false;
+    }
+    if(filter_var($post['email'], FILTER_VALIDATE_EMAIL)){
+        echo ("Votre email n'est pas valide ( format non autorisé )");
+        $valid = false;
+    }
     if ($valid) {
-        if ($post['mdp'] != $post['cmdp']) {
-            echo ("Les champs Mot de passe et Confirmation du mot de passe ne sont pas identiques");
-            return false;
-        } else {
-            $req = $bdd->prepare('INSERT INTO comptes(email, pseudo, password) VALUES(:pscedo, :email, :mdp)');
-            $req->exec($post);
-            return true;
-        }
+        $req = $bdd->prepare('INSERT INTO comptes(email, pseudo, user_password) VALUES(?, ?, ?)');
+        $req->execute([$post['pseudo'], $post['email'], $post['mdp']]);
+        echo ("<span style=\"color:green\">Vous avez bien créer votre compte !</span>");
+        return true;
     } else {
         return false;
     }
