45c50d91d0aba88b2748c13ec3e9777b293b4f71
NonozgYtb <nonozg.ytb@gmail.com>
2020-01-18 20:59:44 +0100

    Correction des fautres d'orthographe

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ require_once("./include.php");
         <div class="col-lg-12 text-center">
           <h2 class="section-heading text-uppercase">Pourquoi Dev'&'Cie ?</h2>
           <h3 class="section-subheading text-muted">Notre entreprise a pour but de satisfaire gratuitement nos clients en leurs offrant des bots discord de qualité.  
-          Nous vous proposons également des services payants qui vous permettront d'avoir vos propres bot personalisés. Vous avez aussi la possibilité de commander des sites internet.</h3>
+          Nous vous proposons également des services payants qui vous permettront d'avoir vos propres bot personnalisés. Vous avez aussi la possibilité de commander des sites internet.</h3>
         </div>
       </div>
       <div class="row text-center">
@@ -60,7 +60,7 @@ require_once("./include.php");
             <i class="fas fa-lock fa-stack-1x fa-inverse"></i>
           </span>
           <h4 class="service-heading">Sécurité oblige !</h4>
-          <p class="text-muted">Comme votre bot et vos sites sont susceptiblent de contenir des informations sensibles, nous garantissons leurs sécurités en emêpchant toute intrusion dans les fichiers. Les bases de données sont cryptés pour garantir une sécurité maximale.</p>
+          <p class="text-muted">Comme votre bot et vos sites sont susceptibles de contenir des informations sensibles, nous garantissons leurs sécurités en empêchant toute intrusion dans les fichiers. Les bases de données sont cryptés pour garantir une sécurité maximale.</p>
         </div>
       </div>
     </div>
@@ -87,7 +87,7 @@ require_once("./include.php");
           </a>
           <div class="portfolio-caption">
             <h4>Free Bot</h4>
-            <p class="text-muted">Un bot multifonction gratuit et de efficace !</p>
+            <p class="text-muted">Un bot multifonction gratuit et efficace !</p>
           </div>
         </div>
         <div class="col-md-4 col-sm-6 portfolio-item">
@@ -101,7 +101,7 @@ require_once("./include.php");
           </a>
           <div class="portfolio-caption">
             <h4>Premium Bot </h4>
-            <p class="text-muted">Accèdez à toutes les commandes VIP grace à Premium Bot !</p>
+            <p class="text-muted">Accèdez à toutes les commandes VIP grâce à Premium Bot !</p>
           </div>
         </div>
         <div class="col-md-4 col-sm-6 portfolio-item">
@@ -115,7 +115,7 @@ require_once("./include.php");
           </a>
           <div class="portfolio-caption">
             <h4>Custom Bot</h4>
-            <p class="text-muted">Avec Custom Bot concevez vos propre bot sur messure !</p>
+            <p class="text-muted">Avec Custom Bot concevez vos propre bot sur mesure !</p>
           </div>
         </div>
         
@@ -128,7 +128,7 @@ require_once("./include.php");
       <div class="row">
         <div class="col-lg-12 text-center">
           <h2 class="section-heading text-uppercase">Notre équipe</h2>
-          <h3 class="section-subheading text-muted">Notre équipe soudée travail en perminnance a vous offrir la qualité !</h3>
+          <h3 class="section-subheading text-muted">Notre équipe soudée travail en permanence a vous offrir la qualité !</h3>
         </div>
       </div>
       <div class="row">
@@ -158,7 +158,7 @@ require_once("./include.php");
                   <h4 class="subheading">Développeur Web & Discord.js</h4>
                 </div>
                 <div class="timeline-body">
-                  <p class="text-muted">Il s'occupe de tout les sites internet ansi que les bots discord </p>
+                  <p class="text-muted">Il s'occupe de tout les sites internet ainsi que les bots discord </p>
                 </div>
               </div>
             </li>
@@ -186,7 +186,7 @@ require_once("./include.php");
                   <h4 class="subheading">Graphiste</h4>
                 </div>
                 <div class="timeline-body">
-                  <p class="text-muted">Vous utiliser notre plateforme, vous nous permettez de vivre de notre passion et en plus de vous mettre a disposition des bots avec des fonctionnalités inédites et exceptionnel !</p>
+                  <p class="text-muted">Vous utilisez notre plateforme, vous nous permettez de vivre de notre passion et en plus de vous mettre a disposition des bots avec des fonctionnalités inédites et exceptionnel !</p>
                 </div>
               </div>
             </li>
