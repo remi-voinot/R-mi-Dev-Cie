f7a0feac5d4b0a72088dd6102ecbcef9011205b9
NonozgYtb <nonozg.ytb@gmail.com>
2020-01-19 14:18:10 +0100

    Modification du Profil + Ajout de lib. CountryFlag

--- dashboard/profile.php
+++ dashboard/profile.php
@@ -52,37 +52,61 @@ require_once("./include.php");
                           <div class="col-12 space-10px md-hide"></div>
                           <div class="input-group input-group-lg col-12 sm-left-rounded-input sm-no-pad">
                             <div class="input-group-prepend sm-hide">
-                              <span class="input-group-text" id="pscedo">Pscedo</span>
+                              <span class="input-group-text" id="pseudo">Pseudo</span>
                             </div>
-                            <input type="text" class="form-control sm-left-rounded-input" aria-label="pscedo" aria-describedby="inputGroup-sizing-lg">
+                            <input type="text" class="form-control sm-left-rounded-input md-placeholder-hide" placeholder="Pseudo" aria-label="pseudo" aria-describedby="inputGroup-sizing-lg">
                           </div>
                           <div class="col-12 space-10px"></div>
                           <div class="input-group input-group-default col-12 col-md-8 sm-left-rounded-input sm-no-pad">
                             <div class="input-group-prepend sm-hide">
                               <span class="input-group-text" id="about">About</span>
                             </div>
-                            <input type="text" class="form-control sm-left-rounded-input" aria-label="about" aria-describedby="inputGroup-sizing-default">
+                            <input type="text" class="form-control sm-left-rounded-input md-placeholder-hide" placeholder="A propos de moi" aria-label="about" aria-describedby="inputGroup-sizing-default">
                           </div>
                           <div class="col-md-4"></div>
+                          <div class="col-12 space-10px"></div>
+                          <div class="input-group mb-3 col-12 sm-left-rounded-input sm-no-pad">
+                            <div class="input-group-prepend sm-hide">
+                              <label class="input-group-text" for="inputGroupSelect01">Pays</label>
+                            </div>
+                            <select class='custom-select' id='states-select'>
+                              <option disabled  >Séléctionnez votre <br>pays de résidence :</option>
+                              <option value='fr' selected>France</option>
+                              <option disabled  >==============</option>
+                              <option value='dz'>Algérie</option>
+                              <option value='de'>Allemagne</option>
+                              <option value='at'>Autriche</option>
+                              <option value='be'>Belgique</option>
+                              <option value='br'>Brésil</option>
+                              <option value='ca'>Canada</option>
+                              <option value='es'>Espagne</option>
+                              <option value='fr'>France</option>
+                              <option value='it'>Italie</option>
+                              <option value='ma'>Maroc</option>
+                              <option value='mx'>Mexique</option>
+                              <option value='nl'>Pays-Bas</option>
+                              <option value='pt'>Portugal</option>
+                              <option value='ru'>Russie</option>
+                              <option value='ch'>Suisse</option>
+                              <option value='tn'>Tunisie</option>
+                              <option value='us'>USA (Etats Unis)</option>
+                              <option value='gb'>United-Kingdoms</option>
+                              <option disabled  >==============</option>
+                              <option value='zz'>Autre</option>
+                            </select>
+                          </div>
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
-
             </div>
-
-
           </div>
-
         </div>
-
       </div>
       <?php footer() ?>
-
     </div>
-
   </div>
   <!-- End of Page Wrapper -->
 
