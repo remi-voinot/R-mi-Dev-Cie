8992f180cf6f74f85613e70e3bfde8d5bbda8782
NonozgYtb <nonozg.ytb@gmail.com>
2020-01-30 22:34:16 +0100

    Petite modification

--- dashboard/balance.php
+++ dashboard/balance.php
@@ -33,8 +33,7 @@ require_once("./include.php");
 
           <!-- Page Heading -->
           <div class="d-sm-flex align-items-center justify-content-between mb-4">
-            <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
-            <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-phone-square-alt fa-sm text-white-50"></i> Aide / Support</a>
+            <h1 class="h3 mb-0 text-gray-800">Solde / Argent</h1>
           </div>
 
           <div class="row">
