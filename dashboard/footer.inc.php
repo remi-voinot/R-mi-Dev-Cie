1ed4b2ad78c613fa7aab67726dddb9313f986524
NonozgYtb <nonozg.ytb@gmail.com>
2020-02-02 00:25:27 +0100

    Modification des mentions légales

--- dashboard/footer.inc.php
+++ dashboard/footer.inc.php
@@ -1,7 +1,7 @@
 <footer class="sticky-footer bg-white">
     <div class="container my-auto">
         <div class="copyright text-center my-auto">
-        <?php require_once("../var.inc.php"); echo(copyright()) ?>
+        <?php require_once("../var.inc.php"); echo(copyright()) ?><span>  -  </span> <a href="/mentionslegales.php">Mentions légales</a>
         </div>
     </div>
 </footer>
--- mentionslegales.php
+++ mentionslegales.php
@@ -20,47 +20,62 @@ require_once("./include.php");
 
 <body id="page-top">
 
-<div class="container-fluid"></div>
-            <div class="row">
-            <div class="col-12">
-            
-             <div class="space-100px"></div>
-                <Div class="display-4 text-secondary text-center">Conditions Dev'&'Cie <br><br><br> </Div>
-                <Div class="ml-5 mr-5 display-4 text-secondary text-center">
+    <div class="container-fluid"></div>
+    <div class="row">
+        <div class="col-12">
 
-Informations <br><br>
+            <div class="space-50px"></div>
+            <div class="display-4 text-secondary text-center">Conditions Dev'&'Cie</div>
+            <div class="ml-5 mr-5 text-secondary text-center display-4-copy">
 
-En vertu de l'article 6 de la loi n° 2004-575 du 21 juin 2004 pour la confiance économique et
-numérique, il est précisé aux utilisateurs du site http://www.devandcie.com l'identité des différents
-intervenants dans le cadre de sa réalisation et de son suivi : Conformément à la loi « informatique et libertés » du 6 janvier 1978 modifiée en 2004, vous bénéficiez d’un droit d’accès et de rectification aux informations qui vous concernent, que vous pouvez exercer en vous adressant à devandcie@gmail.com. <br><br>
+                <div class="space-2em"></div>
+                <span class="dc-2x">Informations</span>
+                <div class="space-2em"></div>
 
-Reproduction<br><br>
+                <span class="dc-1-5x">En vertu de l'article 6 de la loi n° 2004-575 du 21 juin 2004 pour la confiance économique et
+                    numérique, il est précisé aux utilisateurs du site http://www.devandcie.com l'identité des différents
+                    intervenants dans le cadre de sa réalisation et de son suivi : Conformément à la loi « informatique et libertés » du 6
+                    janvier 1978 modifiée en 2004, vous bénéficiez d’un droit d’accès et de rectification aux informations qui vous concernent,
+                    que vous pouvez exercer en vous adressant à devandcie@gmail.com.</span>
 
-L'ensemble de ce site relève des législations internationales sur le droit d'auteur et la propriété intellectuelle. Tous les droits de reproduction sont réservés.<br><br>
+                <div class="space-2em"></div>
+                <span class="dc-2x">Reproduction</span>
+                <div class="space-2em"></div>
 
-Hebergement <br><br>
+                <span class="dc-1-5x">
+                    L'ensemble de ce site relève des législations internationales sur le droit d'auteur et la propriété intellectuelle.
+                    Tous les droits de reproduction sont réservés.</span>
 
-devandcie.com est hébergé par la société OVH.<br><br>
+                <div class="space-2em"></div>
+                <span class="dc-2x">Hebergement</span>
+                <div class="space-2em"></div>
 
-Déclaration <br><br>
+                <span class="dc-1-5x">devandcie.com est hébergé par la société OVH.</span>
 
-Propriétaire : Rémi Voinot <br>
-Responsable publication : Rémi Voinot - +33610678136 <br>
-Le responsable publication est une personne physique ou une personne morale.<br>
-Webmaster : Rémi Voinot<br>
-Hébergeur : OVH – 2 Rue Kellermann 59100 Roubaix 1007<br>
+                <div class="space-2em"></div>
+                <span class="dc-2x">Déclaration</span>
+                <div class="space-2em"></div>
 
-Email: devandcie@gmail.com<br>
 
-Téléphone : +33 6 10 67 81 36 <br><br>
+                <span class="dc-1-5x">Propriétaire : Rémi Voinot <br>
+                    Responsable publication : Rémi Voinot - +33610678136 <br>
+                    Le responsable publication est une personne physique ou une personne morale.<br>
+                    Webmaster : Rémi Voinot<br>
+                    Hébergeur : OVH – 2 Rue Kellermann 59100 Roubaix 1007
 
-<a class="btn btn-primary btn-xl text-uppercase" href="index.php">RETOUR</a>
+                Email: devandcie@gmail.com<br>
 
-                    <div class="space-100px"></div>
-                    </Div>
-                </div>
+                Téléphone : +33 6 10 67 81 36</span>
+
+                <div class="space-2em"></div>
+
+                <a class="btn btn-primary btn-xl text-uppercase" href="index">RETOUR</a>
+
+                <div class="space-50px"></div>
             </div>
-            
+        </div>
+    </div>
+
 
 
     <?php footer() ?>
